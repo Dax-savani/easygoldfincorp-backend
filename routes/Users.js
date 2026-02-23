1d5c11da904da7f67b28531955e6a0aaf8c7a79b
Savani <163087650+Dax-savani@users.noreply.github.com>
2026-02-24 02:12:06 +0530

    Update Users.js

--- routes/Users.js
+++ routes/Users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { addCustomers, allCustomers, addInquiry, allInquiries } = require("../controllers/customers");
 const router = express.Router();
-
+//
 router.post("/newcustomers", addCustomers);
 router.get("/allCustomers", allCustomers);
 router.post("/inquiry", addInquiry);
